6e60a8fd6a60c89c85f677596704648ca9fa1616
Jack <87831546+rootjaxk@users.noreply.github.com>
2021-12-06 19:41:11 +0000

    Update database_conn.php

--- holiday_website/database_conn.php
+++ holiday_website/database_conn.php
@@ -1,6 +1,6 @@
 <?php
    // Create connection with parameters
-   $dbConn = new mysqli('localhost', 'unn_w20007431', 'QTIWODAX', 'unn_w20007431');
+   $dbConn = new mysqli('localhost', 'username', 'password', 'unn_w20007431');
    //check connection
    if ($dbConn->connect_error) {
       echo "<p>Connection failed: ".$dbConn->connect_error."</p>\n";
